4b43d99b84000dbec10cea834612fb67774f7546
Ilkyu Ju <me@ilkyu.kr>
2021-02-22 15:37:09 -0600

    Initialize essential views

---
+++ frontend/src/components/ProfileView.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ProfileView extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <h1>Welcome to ProfileView!</h1>
+    );
+  }
+}
+
+export default ProfileView;
