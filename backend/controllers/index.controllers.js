ca521d343c436137548b7d35efab73a5f05eb43b
Kenneth Mui <klmui@wisc.edu>
2021-02-22 23:47:57 -0600

    change response to json

--- backend/controllers/index.controllers.js
+++ backend/controllers/index.controllers.js
@@ -3,8 +3,8 @@ const db  = require('../models/index.model');
 exports.getMenuAction = (req, res) => {
   // Interact with the model
   db.getMenu().then(data => {
-    console.log(data);
-    res.send(data);
+    //console.log(data);
+    res.json(data);
   }).catch(err => {
     console.log(err);
   });
