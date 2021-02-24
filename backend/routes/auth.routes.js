280c02261f27763fabc6fdef0bdaabd29788f40e
Kenneth Mui <klmui@wisc.edu>
2021-02-24 02:15:23 -0600

    Add functions to for adding an item, updating it, and deleting one

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const authController = require("../controllers/auth.controller.js");
+const authController = require("../controllers/auth.controllers.js");
 const authMiddleware = require("../middleware/auth.middleware");
 
 router.route('/login')
   .post(authController.loginAction);
 
 router.route('/logout')
-  .get(authMiddleware.requireLogin, authController.logoutAction);
+  .get(authMiddleware.requireLogin, authMiddleware.isStaff, authController.logoutAction);
 
 router.route('/signup')
   .post(authController.signupAction);
