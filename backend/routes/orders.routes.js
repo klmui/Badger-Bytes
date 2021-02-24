e93f768d4209d8db1e3ff814e7dcac001cf1c6c3
s-j-cunningham <sjcunningham@wisc.edu>
2021-02-24 15:03:38 -0600

    consolidated orders controllers and models

--- backend/routes/orders.routes.js
+++ backend/routes/orders.routes.js
@@ -3,14 +3,13 @@ const router = express.Router();
 
 // Import functions from controller
 const ordersController = require('../controllers/orders.controller.js');
-const restOrdersController = require('../controllers/rest_orders.controllers.js');
 const authMiddleware = require("../middleware/auth.middleware");
 
 router.route('/orders/:username')
   .get(authMiddleware.requireLogin, ordersController.getOrdersAction);
 
 router.route('/orders')
-  .get(authMiddleware.requireLogin, authMiddleware.isStaff, restOrdersController.getRestOrdersAction);
+  .get(authMiddleware.requireLogin, authMiddleware.isStaff, ordersController.getRestOrdersAction);
 
 
 module.exports = router; // We need this at the end of every route file
