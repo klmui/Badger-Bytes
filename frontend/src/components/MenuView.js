d3baa4c10b655ca5473328640e2bf64a0793463a
Ilkyu Ju <ju.ilkyu@gmail.com>
2021-02-24 00:37:14 -0600

    Cleaning - remove dummy JSON

--- frontend/src/components/MenuView.js
+++ frontend/src/components/MenuView.js
@@ -9,23 +9,6 @@ class MenuView extends Component {
     super(props);
     this.state = {
       menuItems: []
-      // dummy until the API is out
-      // menuItems: [ {foodName: "Crispy Chicken Wings",
-      //               description: "Choice of buffalo, spicy Asian, maple-chipotle, BBQ, honey-harissa, Memphis dry rub or parmesan peppercorn, with celery & housemade blue cheese dressing",
-      //               price: 8.00,
-      //               img_src: "https://via.placeholder.com/500x325",
-      //               quantity: 4},
-      //               {foodName: "Fried Cheese Curds",
-      //               description: "Spotted Cow-battered cheese curds, served with ranch dressing",
-      //               price: 9.00,
-      //               img_src: "https://via.placeholder.com/500x325",
-      //               quantity: 4},
-      //               {foodName: "Hot Soft Pretzels",
-      //               description: "Two fresh baked pretzels with whole grain mustard & jack cheese sauce",
-      //               price: 8.00,
-      //               img_src: "https://via.placeholder.com/500x325",
-      //               quantity: 0}
-      //             ]
     }
   }
 
