6ddabc995d7f13c31138c1c31e68be7f53da524c
Kenneth Mui <klmui@wisc.edu>
2021-02-24 23:08:30 -0600

    signup adjustments

--- frontend/src/components/SignupView.js
+++ frontend/src/components/SignupView.js
@@ -5,15 +5,15 @@ class SignupView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username:"",
-      password:"",
-      address:"",
-      city:"",
-      state:"",
-      zip:"",
-      car_desription:"",
-      type:"",
-      email:""
+      username: null,
+      password:null,
+      address:null,
+      city:null,
+      state:null,
+      zip:null,
+      car_desription:null,
+      type:null,
+      email:null
     }
     this.submitForm = this.submitForm.bind(this);
   }
@@ -41,18 +41,18 @@ class SignupView extends Component {
           <Form.Row >
             <Form.Group as={Col} controlId="formGridEmail" style={{paddingTop:"10px",paddingLeft:"20px"}}>
               <Form.Label>Username</Form.Label>
-              <Form.Control type="username" placeholder="Enter username" value={this.state.username} onChange={e => this.setState({username: e.target.value})}/>
+              <Form.Control type={"text"} placeholder="Enter username" value={this.state.username} onChange={e => this.setState({username: e.target.value})}/>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridPassword" style={{paddingTop:"10px", paddingLeft:"10px", paddingRight:"20px" }}>
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" value={this.state.password} onChange={e => this.setState({password: e.target.value})}/>
+              <Form.Control type={"password"} placeholder="Password" value={this.state.password} onChange={e => this.setState({password: e.target.value})}/>
             </Form.Group>
           </Form.Row>
           <Form.Row>
           <Form.Group as={Col} controlId="formGridAddress1"  style={{paddingLeft:"20px", paddingRight:"20px"}}>
             <Form.Label>Address</Form.Label>
-            <Form.Control placeholder="1234 Main St" value={this.state.address} onChange={e => this.setState({address: e.target.value})}/>
+            <Form.Control type={"text"} placeholder="1234 Main St" value={this.state.address} onChange={e => this.setState({address: e.target.value})}/>
           </Form.Group>
           </Form.Row>
           
@@ -60,26 +60,26 @@ class SignupView extends Component {
           <Form.Row>
             <Form.Group as={Col} controlId="formGridCarDesc" style={{paddingLeft:"20px", paddingRight:"20px"}}>
                 <Form.Label>Car Description</Form.Label>
-                <Form.Control type="car_description" value={this.state.car_desription} onChange={e => this.setState({car_desription: e.target.value})} />
+                <Form.Control type={"text"} value={this.state.car_desription} onChange={e => this.setState({car_description: e.target.value})} />
               </Form.Group>
           </Form.Row>
 
           <Form.Row>
             <Form.Group as={Col} controlId="formControlsEmail" style={{paddingLeft:"20px", paddingRight:"20px"}}>
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" value={this.state.email} onChange={e => this.setState({email: e.target.value})}/>
+                <Form.Control type={"email"} value={this.state.email} onChange={e => this.setState({email: e.target.value})}/>
               </Form.Group>
           </Form.Row>
 
           <Form.Row>
             <Form.Group as={Col} controlId="formGridCity" style={{paddingLeft:"20px"}}>
               <Form.Label>City</Form.Label>
-              <Form.Control type="city" value={this.state.city} onChange={e => this.setState({city: e.target.value})}/>
+              <Form.Control type={"text"} value={this.state.city} onChange={e => this.setState({city: e.target.value})}/>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridState">
               <Form.Label>State</Form.Label>
-              <Form.Control type="state"value={this.state.state} onChange={e => this.setState({state: e.target.value})}/>
+              <Form.Control type={"text"} value={this.state.state} onChange={e => this.setState({state: e.target.value})}/>
             </Form.Group>
 
             <Form.Group as={Col} controlId="formGridZip" style={{paddingRight:"20px"}}>
