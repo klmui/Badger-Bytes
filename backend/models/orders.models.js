01aa0cadf007664fdee253f4dd0ec73d7f0a4de7
Kenneth Mui <klmui@wisc.edu>
2021-02-26 02:21:56 -0600

    for ilkyu

--- backend/models/orders.models.js
+++ backend/models/orders.models.js
@@ -11,16 +11,14 @@ exports.getOrders = (req, res) => {
             o.id as order_id,
             o.order_date_time, 
             o.pickup_date_time, 
-            o.payment_id, 
+            o.payment_type, 
             o.completed,
             o.username, 
-            p.payment_type, 
             s.food_id, 
             f.name as food_name, 
             s.quantity_served, 
             f.price as unit_price
           FROM user_order o
-          JOIN user_payment p ON o.payment_id = p.id
           JOIN served s ON s.user_order_id = o.id
           JOIN food f ON s.food_id = f.id
           WHERE o.username = ?
@@ -58,16 +56,14 @@ exports.getRestOrders = (req, res) => {
             o.id as order_id,
             o.order_date_time, 
             o.pickup_date_time, 
-            o.payment_id, 
+            o.payment_type, 
             o.completed,
             o.username, 
-            p.payment_type, 
             s.food_id, 
             f.name as food_name, 
             s.quantity_served, 
             f.price as unit_price
             FROM user_order o
-            JOIN user_payment p ON o.payment_id = p.id
             JOIN served s ON s.user_order_id = o.id
             JOIN food f ON s.food_id = f.id
             ORDER BY o.order_date_time DESC;
@@ -86,3 +82,31 @@ exports.getRestOrders = (req, res) => {
       });
     });
 }
+
+// exports.submitOrder = (req, res) => {
+//   return new Promise((resolve, reject) => {
+//     // Connect to database
+//     pool.getConnection((err, connection) => {
+//       let query = `
+//         INSERT INTO user_order (order_date_time, pickup_date_time, payment_type, completed, username) 
+//         VALUES ?
+//       `;
+//       let values = [
+//         [req.body.orderDatTime, req.body.pickupDateTime, req.body.paymentType, req.body.completed. req.body.username]
+//       ];
+//       connection.query(query, [values], (error, rows, fields) => {
+//         if (error) {
+//           console.log("Error in query!");
+//           reject(err);
+//         } else {
+//           const orderId = rows.insertId;
+//           query = `
+//             INSERT INTO served (food_id, quantity_served, user_order_id)
+//             VALUES ?
+//           `;
+//           values = [];
+//         }
+//       });
+//     });
+//   });
+// }
