688d82f6fa888a9909d6fbe7825309190aa0a55b
Ilkyu Ju <ju.ilkyu@gmail.com>
2021-02-26 03:28:30 -0600

    Show Date and Time in human readable form

--- frontend/src/components/OrderItem.js
+++ frontend/src/components/OrderItem.js
@@ -14,12 +14,39 @@ class OrderItem extends Component {
     super(props);
   }
 
+  // converts Date ISO String to human readable time
+  showTime() {
+    return new Date(
+        this.props.orderItem.pickup_date_time
+      ).toLocaleTimeString(
+          // omit seconds
+          navigator.language, {
+            hour: '2-digit',
+            minute: '2-digit',
+      });
+  }
+
+  // converts Date ISO String to human readable date
+  showDate() {
+    return new Date(
+        this.props.orderItem.pickup_date_time
+      ).toLocaleDateString()
+  }
+
+
   render() {
     return (
       <Card className="col-md-8">
         <Card.Body>
           <Card.Title>
-              <Row>Order #{this.props.orderItem.order_id}</Row>
+              <Row className="justify-content-between">
+                <Col>
+                  Order #{this.props.orderItem.order_id}
+                </Col>
+                <Col className="text-right">
+                  {this.showDate()}
+                </Col>
+              </Row>
               <Row className="justify-content-center align-center align-items-top">
                 <Cart cartItems={this.props.orderItem.foods} checkoutMode />
                 {/* <Col xs={2}> {(this.props.or.quantity > 0) && <Button onClick={this.addToCart} size="sm"><AiOutlinePlus /></Button> }</Col> */}
@@ -29,12 +56,16 @@ class OrderItem extends Component {
             <Row className="justify-content-between align-center">
               <Col>
                 <Button block variant="light" disabled>Pickup Time</Button>
-                <Button block variant="outline-primary" disabled>{this.props.orderItem.pickup_date_time}</Button>
+                <Button block variant="outline-primary" disabled>{this.showTime()}</Button>
               </Col>
               <Col>
+              <Button block variant="light" disabled>Car Description</Button>
+                <Button block variant="outline-primary" disabled>TODO: Currently there's no way to access car description from the API</Button>
+              </Col>
+            </Row>
+            <Row style={{marginTop: "1rem"}} className="justify-content-between align-center">
                 <Button block variant="success">Print Order</Button>
                 <Button block variant="danger">Complete Order</Button>
-              </Col>
             </Row>
           </Card.Text>
         </Card.Body>
