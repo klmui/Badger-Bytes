2617e64b0e54f91beb1ec93071c6f19de83e11a8
Ilkyu Ju <ju.ilkyu@gmail.com>
2021-02-26 03:00:02 -0600

    Add OrderService

---
+++ frontend/src/services/order.service.js
@@ -0,0 +1,28 @@
+import request from './request'
+
+function getAllOrders() {
+    return request({
+        url: '/orders',
+        method: 'GET',
+        withCredentials: true,
+    })
+}
+
+function getUserOrders(username) {
+    return request({
+        url: '/orders/username',
+        method: 'GET',
+        withCredentials: true,
+    })
+}
+
+// TODO
+function getDetail(id) {
+
+}
+
+const MenuService = {
+    getAllOrders, getUserOrders, getDetail
+}
+
+export default MenuService;
